d6db168c69310c071680d8ecf06a7914c9936000
Eduardo Leite <eduardoleite@Eduardos-MacBook-Air.local>
2024-08-19 19:16:29 -0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+import Auth from './auth.js';
+const Router = (server) => {
+  // home route with the get method and a handler
+  server.use('/auth', Auth);
+  server.get("/", (req, res) => {
+      try {
+          res.status(200).json({
+              status: "success",
+              data: [],
+              message: "Welcome to our API homepage!",
+          });
+      } catch (err) {
+          res.status(500).json({
+              status: "error",
+              message: "Internal Server Error",
+          });
+      }
+  })
+  };
+  export default Router;
